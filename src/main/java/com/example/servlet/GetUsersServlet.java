86a69086f683db4941abe2afc452a9a4a09ce96f
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-14 21:19:38 +0300

    completed

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,23 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@WebServlet("/users")
+public class GetUsersServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       Set<User> allUsers = Warehouse.getInstance().getUsers();
+       req.setAttribute("users", allUsers);
+        req.getRequestDispatcher("/jsp/users.jsp").forward(req,resp);
+    }
 }
